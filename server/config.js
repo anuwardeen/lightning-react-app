ce35205d31db0c6e345bcbb40de6060d3cff48b0
anuwardeen <anuwardeen@gmail.com>
2016-07-15 16:28:44 +0530

    Update config.js
    de url update
    
    from "postgres://ccoenraets@localhost/realty" to "postgres://lzmkfcjq:Z5Y_3BIm4wcZxV8Fn7o81mqA1DBJNTdg@pellefant.db.elephantsql.com:5432/lzmkfcjq"

--- server/config.js
+++ server/config.js
@@ -1,5 +1,5 @@
 module.exports = {
 
-    databaseURL: process.env.DATABASE_URL || "postgres://ccoenraets@localhost/realty"
+    databaseURL: process.env.DATABASE_URL || "postgres://lzmkfcjq:Z5Y_3BIm4wcZxV8Fn7o81mqA1DBJNTdg@pellefant.db.elephantsql.com:5432/lzmkfcjq"
 
 };
