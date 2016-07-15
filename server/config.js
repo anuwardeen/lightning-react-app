d5a2700ad1cca9ba190e35634e2aa61850a17b55
anuwardeen <anuwardeen@gmail.com>
2016-07-15 16:31:53 +0530

    Update config.js
    new url : postgres://uerrzcos:nwW8NLZIYaz1noStPkVreYyuL3CE73HK@babar.elephantsql.com:5432/uerrzcos

--- server/config.js
+++ server/config.js
@@ -1,5 +1,5 @@
 module.exports = {
 
-    databaseURL: process.env.DATABASE_URL || "postgres://lzmkfcjq:Z5Y_3BIm4wcZxV8Fn7o81mqA1DBJNTdg@pellefant.db.elephantsql.com:5432/lzmkfcjq"
+    databaseURL: process.env.DATABASE_URL || "postgres://uerrzcos:nwW8NLZIYaz1noStPkVreYyuL3CE73HK@babar.elephantsql.com:5432/uerrzcos"
 
 };
